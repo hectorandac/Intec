d784855cc9b905a30c9f9a3fa5453c049370902d
hectorandac01 <hectorcuriso4@gmail.com>
2016-08-26 23:37:55 -0400

    HectorAndAc

--- app/src/main/java/com/dragon/intec/objects/Student.java
+++ app/src/main/java/com/dragon/intec/objects/Student.java
@@ -11,6 +11,11 @@ import android.util.Log;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.channels.AsynchronousCloseException;
 import java.util.ArrayList;
 
@@ -18,89 +23,83 @@ public class Student {
 
     private String token;
     private String secret;
-    private ArrayList<Object> information = new ArrayList<>();
+    private ArrayList<Object> information;
     private static final String keyToken = "TOKEN";
     private static final String keySecret = "SECRETE";
     private static final String keyObject = "STUDENT";
+    private Activity activity;
 
     public boolean internetConnection = false;
 
     public Student(String token, String secret, Activity activity){
         this.token = token;
         this.secret = secret;
+        this.activity = activity;
 
         SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
-        new getData().execute(token, secret, activity);
-
         editor.putString(keyToken, token);
         editor.putString(keySecret, secret);
+
         editor.apply();
     }
 
-    private class getData extends AsyncTask<Object, Void, Void> {
+    public ArrayList<Object> getData() throws IOException {
 
-        @Override
-        protected Void doInBackground(Object... objects) {
+        information = new ArrayList<>();
 
-            Activity activity = (Activity)objects[2];
+        if(!internetConnection){
 
-            if(!internetConnection){
-                SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
-                String jsonOBJ = sharedPref.getString(keyObject, "");
-                JSONObject jsonObject = null;
-                try {
-                    jsonObject = new JSONObject(jsonOBJ);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                assert jsonObject != null;
-                information.add(jsonObject.optString("id"));
-                information.add(jsonObject.optString("name"));
-                information.add(jsonObject.optString("academic_condition"));
-                information.add(jsonObject.optString("quarter"));
-                information.add(jsonObject.optString("last_condition"));
-                information.add(jsonObject.optString("quarter_index"));
-                information.add(jsonObject.optString("general_index"));
-                information.add(jsonObject.optString("validated_credits"));
-                information.add(jsonObject.optString("approved_credits"));
-                information.add(jsonObject.optString("approved_quarters"));
-                information.add(jsonObject.optString("alerts"));
-            }else{
-                //Get json from server parse it and add it to the list
-                JSONObject jsonObject = null;
-                try {
-                    jsonObject = new JSONObject("-----------");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                assert jsonObject != null;
-                information.add(jsonObject.optString("id"));
-                information.add(jsonObject.optString("name"));
-                information.add(jsonObject.optString("academic_condition"));
-                information.add(jsonObject.optString("quarter"));
-                information.add(jsonObject.optString("last_condition"));
-                information.add(jsonObject.optString("quarter_index"));
-                information.add(jsonObject.optString("general_index"));
-                information.add(jsonObject.optString("validated_credits"));
-                information.add(jsonObject.optString("approved_credits"));
-                information.add(jsonObject.optString("approved_quarters"));
-                information.add(jsonObject.optString("alerts"));
-                //Then save it
-                saveToJSON(activity);
+            SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+            String jsonOBJ = sharedPref.getString(keyObject, "");
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject(jsonOBJ);
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
 
+            assert jsonObject != null;
+            information.add(jsonObject.optString("id"));
+            information.add(jsonObject.optString("name"));
+            information.add(jsonObject.optString("academic_condition"));
+            information.add(jsonObject.optString("quarter"));
+            information.add(jsonObject.optString("last_condition"));
+            information.add(jsonObject.optString("quarter_index"));
+            information.add(jsonObject.optString("general_index"));
+            information.add(jsonObject.optString("validated_credits"));
+            information.add(jsonObject.optString("approved_credits"));
+            information.add(jsonObject.optString("approved_quarters"));
+            information.add(jsonObject.optString("alerts"));
+        }else{
+            //Testing
+            URL url = new URL("http://coolsite.com/coolstuff.js");
+            InputStream in = url.openStream();
+            InputStreamReader reader = new InputStreamReader(in);
+            //Get json from server parse it and add it to the list
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject("-----------");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
-            return null;
+            assert jsonObject != null;
+            information.add(jsonObject.optString("id"));
+            information.add(jsonObject.optString("name"));
+            information.add(jsonObject.optString("academic_condition"));
+            information.add(jsonObject.optString("quarter"));
+            information.add(jsonObject.optString("last_condition"));
+            information.add(jsonObject.optString("quarter_index"));
+            information.add(jsonObject.optString("general_index"));
+            information.add(jsonObject.optString("validated_credits"));
+            information.add(jsonObject.optString("approved_credits"));
+            information.add(jsonObject.optString("approved_quarters"));
+            information.add(jsonObject.optString("alerts"));
+            //Then save it
+            saveToJSON(activity);
         }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            Log.i("INFO_user", (String)information.get(1));
-        }
+        return information;
     }
 
     public void saveToJSON(Activity activity){
